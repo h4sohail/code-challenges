92a584aafaeb855c2ca59368a6377e5dcf39819e
Hamza <hamza.sohail@carleton.ca>
2019-11-01 09:53:20 -0400

    vector array

--- ccss_challenges/largest_in_array.cpp
+++ ccss_challenges/largest_in_array.cpp
@@ -1,5 +1,5 @@
 /*
-Write a function that takes an array of numbers as input and outputs the 
+Write a function that takes an array of numbers as input and outputs the
 maximum value in the array.
 You may assume that the input array will always have a length greater than 0.
 Example:
@@ -12,28 +12,20 @@ Output: 10
 #include <iostream>
 #include <vector>
 
-int arr[] = { 1, 4, 2, 7, 10, 8 };
+std::vector<int> arr = { 1, 4, 2, 7, 10, 8 };
 
-int main() 
-{	
-	std::vector<int> vec;
-	size_t size = (sizeof(arr) / sizeof(arr[0]));
-
-	for (size_t i = 0; i < size; i++) 
+int main()
+{
+	while (!(arr.size() == 1))
 	{
-		vec.push_back(arr[i]);
-	}
-
-	while (!(vec.size() == 1)) 
-	{
-		if (vec[1] > vec[0])
-			vec.erase(vec.begin());
+		if (arr[1] > arr[0])
+			arr.erase(arr.begin());
 		else
-			vec.erase(vec.begin() + 1);
+			arr.erase(arr.begin() + 1);
 	}
 
-	int largestNumber = vec[0];
+	int largestNumber = arr[0];
 	std::cout << largestNumber;
-	
+
 	return largestNumber;
 }
