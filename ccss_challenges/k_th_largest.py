db11580b603f277e632dcba6061f48cc5467c98d
h4sohail <hamza.sohail@carleton.ca>
2019-11-18 15:16:41 -0500

    k_largest fix

---
+++ ccss_challenges/k_th_largest.py
@@ -0,0 +1,13 @@
+lst = [1,5,4,9,2]
+k = 1
+def k_largest(lst, k):
+    counter = 0
+    for i in range(len(lst)):
+        counter += 1
+        for j in range(len(lst)-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+        if counter == k:
+            return lst[-k] 
+
+print(k_largest(lst, k))    
